e56106040848ee93d6445537f1a38e378a9407b2
Flo <florian.schildwaechter@bauerxcel.de>
2020-06-17 16:40:34 +0200

    feat: added script for generation of registry.json

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+const registry = 'registry.json'
+
+const writeRegistry = () => {
+  const modulesDirectory = path.resolve('sloth_modules')
+  const moduleDirectories = fs.readdirSync(modulesDirectory)
+
+  const registryData = moduleDirectories.map((directory, i) => {
+    const configFile = path.resolve(modulesDirectory, directory, 'config.json')
+
+    if (fs.pathExistsSync(configFile)) {
+      const configObject = fs.readJSONSync(configFile)
+      return { ...configObject }
+    }
+  })
+
+  fs.writeJSONSync(registry, registryData)
+  console.log('created registry.js')
+}
+
+writeRegistry()
